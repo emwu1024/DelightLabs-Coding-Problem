a150e981ef9b3d37cd7c3dc9dbe13b28be975c1e
Emily <emwu1024@gmail.com>
2021-05-12 15:31:29 +0100

    Improve trigram subroutine and catch more edge cases

--- trigram.py
+++ trigram.py
@@ -1,30 +1,35 @@
-def trigram(inputStr):
+def trigramFirst(inputStr):
     """
-    Trigram subroutine that takes an input string
+    First attempt Trigram subroutine that takes an input string
     splits the input string and stores each element in a list
     then outputs groups of three consecutive elements for all except the last two
     """
     splitStr = inputStr.split()
     for i in range(len(splitStr)-2):
         print(splitStr[i] + " " + splitStr[i+1] + " " + splitStr[i+2])
+        
 
-def ngram(inputStr, n):
+def trigram (inputStr):
+    """
+    Improved trigram subroutine that calls ngram with inputStr and 3 as params
+    """
+    ngram(inputStr, 3)
+
+
+def ngram(inputStr, n: int):
     """
     n-gram subroutine that takes an input string
     splits the input string and stores each element in a list
     then for every element it concatenates n consecutive elements to the output string
+    assumption: user can't enter negatives or non-ints, n value greater than number of words in n-gram would not be valid
     """
     splitStr = inputStr.split()
-    if (n <= len(splitStr)):
+    if ((n <= len(splitStr)) and (n > 0)):
         for i in range(len(splitStr)-n+1):
-            outStr = ""
-            for j in range(n):
-                outStr = outStr + " " + splitStr[i+j]
+            outStr = splitStr[i]
+            for j in range(n-1):
+                outStr = outStr + " " + splitStr[i+j+1]
             print(outStr)
     else:
         print("No n-grams found")
-    
-
-def main():
-    trigram("Little Red Riding Hood is walking in the forest")
             
